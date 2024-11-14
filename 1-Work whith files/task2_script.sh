978f14cbbf88d124b447b21c38fb6e512aa8d30c
arapov <arapov-5@mail.ru>
2024-11-14 17:55:18 +0400

    Rename.

--- 1-Work whith files/task2_script.sh
+++ 1-Work whith files/task2_script.sh
@@ -5,10 +5,10 @@ cd task_2
 pwd
 
 # 4) Создать файл с содержимым не используя текстовые редактор
-echo "Soderjimoe." > file1
+echo "Soderjimoe." | cat  > file1
 
 # 3) Вывести содержание файла не используя текстовые редакторы.
-file1 > 1
+cat file1
 
 cd ..
 rm -rf task_2
