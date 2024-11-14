4def6c19bbaeb392d2652feeead2c0d800e16fcf
arapov <arapov-5@mail.ru>
2024-11-14 19:28:41 +0400

    (5).

--- 1-Work whith files/task2_script.sh
+++ 1-Work whith files/task2_script.sh
@@ -5,10 +5,11 @@ cd task_2
 pwd
 
 # 4) Создать файл с содержимым не используя текстовые редактор
-echo "Soderjimoe." | cat  > file1
+echo "Soderjimoe." | cat > file1
 
-# 3) Вывести содержание файла не используя текстовые редакторы.
+# 3) Вывести содержание файла не используя текстовые редакторы
 cat file1
 
-cd ..
-rm -rf task_2
+# 5) Перенаправить stdout в stderr и обратно на примере команды kinit, ping, tracert
+ping -c 2 vk.com 1>&2 1> file2 2>&1
+cat file2
