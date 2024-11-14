2a705d8c40d4a0eda42e4c19f0e718b97fcb3651
arapov <arapov-5@mail.ru>
2024-11-14 19:41:02 +0400

    Maybe final.

--- 1-Work whith files/task2_script.sh
+++ 1-Work whith files/task2_script.sh
@@ -11,5 +11,8 @@ echo "Soderjimoe." | cat > file1
 cat file1
 
 # 5) Перенаправить stdout в stderr и обратно на примере команды kinit, ping, tracert
-ping -c 2 vk.com 1>&2 1> file2 2>&1
+ping -c 1 vk.com 1>&2 1> file2 2>&1
 cat file2
+
+# 8) Как отправить весь вывод команды в пустоту?
+ping -c 1 vk.com 1> /dev/null
